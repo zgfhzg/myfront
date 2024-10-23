56bf534509ae9cc94cb24d99d799a51453f6bf66
zgfhzg <43362359+zgfhzg@users.noreply.github.com>
2024-10-23 17:51:56 +0900

    Update table.tsx
    총합 구하는 로직 추가

--- components/table.tsx
+++ components/table.tsx
@@ -7,10 +7,11 @@ import '../public/css/table.css';
 interface TableProps<T extends object> {
     columns: Column<T>[];
     data: T[];
+    totalPrice: number;
     tableId?: string;
 }
 
-const Table = <T extends object>({ columns, data, tableId }: TableProps<T>) => {
+const Table = <T extends object>({ columns, data, totalPrice, tableId }: TableProps<T>) => {
     const {
         getTableProps,
         getTableBodyProps,
@@ -22,26 +23,32 @@ const Table = <T extends object>({ columns, data, tableId }: TableProps<T>) => {
     return (
         <table {...getTableProps()} id={tableId}>
             <thead>
-            {headerGroups.map((headerGroup, index) => (
-                <tr {...headerGroup.getHeaderGroupProps()} key={index}>
-                    {headerGroup.headers.map((column, j) => (
-                        <th {...column.getHeaderProps()} key={j}>{column.render('Header')}</th>
-                    ))}
-                </tr>
-            ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-            {rows.map((row, index) => {
-                prepareRow(row);
-                return (
-                    <tr {...row.getRowProps()} key={index}>
-                        {row.cells.map((cell, j) => (
-                            <td {...cell.getCellProps()} key={j}>{cell.render('Cell')}</td>
+                {headerGroups.map((headerGroup, index) => (
+                    <tr {...headerGroup.getHeaderGroupProps()} key={index}>
+                        {headerGroup.headers.map((column, j) => (
+                            <th {...column.getHeaderProps()} key={j}>{column.render('Header')}</th>
                         ))}
                     </tr>
-                );
-            })}
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, index) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()} key={index}>
+                            {row.cells.map((cell, j) => (
+                                <td {...cell.getCellProps()} key={j}>{cell.render('Cell')}</td>
+                            ))}
+                        </tr>
+                    );
+                })}
             </tbody>
+            <tfoot>
+                <tr>
+                    <td>합계</td>
+                    <td colSpan={3}>{totalPrice}</td>
+                </tr>
+            </tfoot>
         </table>
     );
 };

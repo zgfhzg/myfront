6edb0c9a3ef44d9f67117d3530dbf0330a5749ec
zgfhzg <43362359+zgfhzg@users.noreply.github.com>
2024-10-23 17:52:25 +0900

    Update page.tsx
    계산 로직 적용

--- app/wedding/budget/page.tsx
+++ app/wedding/budget/page.tsx
@@ -2,29 +2,32 @@ import Table from "@/components/table";
 import {Column} from "react-table";
 
 interface Data {    // 데이터 구조체
+    title: string;
+    amount: number;
+    date: string;
     name: string;
-    age: number;
-    city: string;
 }
 
 const data: Data[] = [  // 실제 데이터 값
-    { name: 'John', age: 28, city: 'New York' },
-    { name: 'Jane', age: 22, city: 'San Francisco' },
-    { name: 'Mike', age: 32, city: 'Chicago' },
+    { title: '계약금', amount: 1500000 , date: '24/09/01', name: 'SH' },
+    { title: '사진', amount: 500000, date: '24/09/20', name: 'SH' },
+    { title: '비행기', amount: 5000000, date: '24/10/22', name: 'SH' },
 ];
 
 const columns: Column<Data>[] = [
-    { Header: 'Name', accessor: 'name' },
-    { Header: 'Age', accessor: 'age' },
-    { Header: 'City', accessor: 'city' },
+    { Header: '항목', accessor: 'title' },
+    { Header: '금액', accessor: 'amount' },
+    { Header: '날짜', accessor: 'date' },
+    { Header: '결제자', accessor: 'name' },
 ];
+const totalPrice = data.reduce((sum, row) => sum + row.amount, 0);
 
 export default function Budget() {
     return (
         <div id="main">
             <div className="inner">
                 <h1 id="pageTitle" className="subtitle">Budget Calculation</h1>
-                <Table tableId="budgetTable" columns={columns} data={data} />
+                <Table tableId="budgetTable" columns={columns} data={data} totalPrice={totalPrice} />
             </div>
         </div>
     )

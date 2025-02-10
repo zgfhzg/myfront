4c0a79ee7d6e58f30fc5f5fa3ec2302cb43d1ced
seokhyun <zgfhzg@gmail.com>
2025-02-10 22:00:28 +0900

    api 요청 경로 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/:path*",
+                destination: "http://localhost/:path*",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
